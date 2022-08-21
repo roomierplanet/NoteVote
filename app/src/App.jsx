05550df1ddf2a0b27049cb12709f995e0ca17956
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2022-08-21 17:50:16 -0400

    Added vote link

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,6 +5,8 @@ import Host from './Routes/Host/Host'
 import HostSignup from './Routes/HostSignup/HostSignup';
 import HostDashboard from './Routes/HostDashboard/HostDashboard';
 import User from './Routes/User/User';
+import HostView from './Routes/HostView/HostView';
+import HostVote from './Routes/HostVote/HostVote';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
           <Route exact path="/host/signup" element={<HostSignup />} />
           <Route exact path="/host/dashboard" element={<HostDashboard />} />
           <Route exact path='/user' element={<User />} />
+          <Route exact path='/user/viewhost/:id' element={<HostView />} />
+          <Route exact path='/user/votehost/:id' element={<HostVote />} />
       </Routes>
     </Router> 
   );
