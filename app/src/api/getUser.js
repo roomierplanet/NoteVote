b7b9f966653f7a4423e191f960d66ef2997f88a9
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2022-08-21 17:52:52 -0400

    Added user api

--- app/src/api/getUser.js
+++ app/src/api/getUser.js
@@ -1,2 +1,29 @@
 // The user information is set on localStorage. This is better since user authentication adds a useless step and takes up more 
 // cloud infrastructure. Will consider adding Auth0 as application scales.
+const getUser = {
+    getUser() {
+        let user_info = localStorage.getItem('user_info', '');
+        if (!user_info) {
+            const body = {
+                'joined on': (new Date()),
+                'votes': {},
+            }
+            localStorage.setItem('user_info', JSON.stringify(body));
+        }
+        return JSON.parse(localStorage.getItem('user_info', ''));
+    },
+    canVote(hostId) {
+        const user_info = this.getUser();
+        const last_vote = user_info.votes[hostId];
+        if (last_vote) {
+            const time_now = new Date();
+            const hours = 1000 * 60 * 60;
+            const last_vote_time = new Date(last_vote);
+            const hours_passed = (time_now - last_vote_time) / hours;
+            return hours_passed >= 24;
+        }
+        return true;
+    }
+}
+
+export default getUser;
