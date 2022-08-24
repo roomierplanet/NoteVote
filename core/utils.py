56d5ff5df13fc6042fae445e3e180fc5f93a81b3
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2022-08-24 11:29:49 -0400

    Refactored to use env file

--- core/utils.py
+++ core/utils.py
@@ -1,9 +1,12 @@
 from pymongo import MongoClient
+from backend import settings
 
-client = MongoClient('mongodb+srv://arnavnagpal:L0FKHBfmgpWZShHo@note-vote.fin7qf1.mongodb.net/?retryWrites=true&w=majority')
+client_url = 'mongodb+srv://arnavnagpal:' + settings.DB_PASSWORD + '@note-vote.fin7qf1.mongodb.net/?retryWrites=true&w=majority'
+
+client = MongoClient(client_url)
 
 db = client.get_database('NoteVote')
 
+# collection with group(host) data
 group_records = db.Groups
 
-# add user records later
