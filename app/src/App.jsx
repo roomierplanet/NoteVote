e2040e60975cefbf5f0951a3cd790f43ac606350
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2022-09-10 21:27:02 -0400

    Added logo on top

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -8,16 +8,24 @@ import HostView from './Routes/HostView/HostView';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route exact path="/host" element={<Host />} />
-          <Route exact path="/host/signup" element={<HostSignup />} />
-          <Route exact path="/host/dashboard" element={<HostDashboard />} />
-          <Route exact path='/user' element={<User />} />
-          <Route exact path='/user/viewhost/:id' element={<HostView />} />
-      </Routes>
-    </Router> 
+    <>
+      <img src="/favicon.ico" alt="logo-notevote" height="50px" style={{
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        display: 'block',
+        marginTop: '2rem'
+      }}/>
+      <Router>
+        <Routes>
+            <Route exact path="/" element={<LandingPage />} />
+            <Route exact path="/host" element={<Host />} />
+            <Route exact path="/host/signup" element={<HostSignup />} />
+            <Route exact path="/host/dashboard" element={<HostDashboard />} />
+            <Route exact path='/user' element={<User />} />
+            <Route exact path='/user/viewhost/:id' element={<HostView />} />
+        </Routes>
+      </Router>
+    </> 
   );
 }
 
